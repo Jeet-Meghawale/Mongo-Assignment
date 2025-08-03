cd227cf8cec6206b2909a556b280dabc3a75d6ad
Jeet-Meghwale <jeetmeghraj@gmail.com>
2025-08-03 13:05:11 +0530

    added /admin/cources route with Zod input validation

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,17 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-const { Admin } = require("../db")
+const { Admin, Courses } = require("../db")
+const { z } = require("zod");
+
+
+const courseSchema = z.object({
+    title: z.string().min(1, "Title is required"),
+    description: z.string().min(1, "Description is required"),
+    imgLink: z.string().url("Must be a valid URL"),
+    price: z.number().positive("Price must be a positive number")
+});
+
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
@@ -26,8 +36,34 @@ router.post('/signup', (req, res) => {
 
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    try {
+        const parseData = courseSchema.parse(req.body);
+        const { title, description, imgLink, price } = parseData;
+
+        const newCourse = await Courses.create({
+            title,
+            description,
+            imgLink,
+            price
+        });
+
+        res.json({
+            message: 'Course created successfully', courseId: newCourse._id
+        })
+    } catch (err) {
+        console.error("Error details:", err);  // 🔍 View actual error
+
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ error: err.errors });
+        }
+
+        res.status(500).json({ error: "Internal server error" });
+    }
+
+
+
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
